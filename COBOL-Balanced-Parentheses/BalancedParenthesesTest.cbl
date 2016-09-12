d6aee0185b0242f2afd05a0b9b892db119609a3a
Mike B Harris <mike.harris@xtreamlab.net>
2016-09-12 22:32:17 +0100

    Removed spaces from comment description

--- COBOL-Balanced-Parentheses/BalancedParenthesesTest.cbl
+++ COBOL-Balanced-Parentheses/BalancedParenthesesTest.cbl
@@ -5,16 +5,11 @@ program-id. BalancedParenthesesTest.
 *> the brackets [], and the braces {}, in a string are balanced.
 
 *> For example:
-
 *> {{)(}} is not balanced because ) comes before (
-
 *> ({)} is not balanced because ) is not balanced between {}
      *> and similarly the { is not balanced between ()
-
 *> [({})] is balanced
-
 *> {}([]) is balanced
-
 *> {()}[[{}]] is balanced
 
 environment division.
