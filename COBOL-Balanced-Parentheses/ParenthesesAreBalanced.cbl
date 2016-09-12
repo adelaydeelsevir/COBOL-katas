69c32fd1ec338624a58b967690b0d8092cd42b49
Mike B Harris <mike.harris@xtreamlab.net>
2016-09-12 22:31:57 +0100

    Refactored code to make it more readable

--- COBOL-Balanced-Parentheses/ParenthesesAreBalanced.cbl
+++ COBOL-Balanced-Parentheses/ParenthesesAreBalanced.cbl
@@ -8,17 +8,21 @@ configuration section.
 
     special-names.
         class OpenRoundBracket is "("
-        class CloseRoundBracket is "("
+        class CloseRoundBracket is ")"
         class OpenSquareBracket is "["
         class CloseSquareBracket is "]"
         class OpenBrace is "{"
-        class CloseBrace is "}".
+        class CloseBrace is "}"
+        class Opener is "(", "[", "{"
+        class Closer is ")", "]", "}".
 
 data division.
 local-storage section.
     01 NumCharsInString pic 99 value zero.
     01 CurrentCharacterPosition pic 99 value zero.
     01 LastUnclosedOpenerPosition pic 99 value zero.
+    01 CurrentCharacter pic x value space.
+    01 LastOpenerCharacter pic x value space.
 
 linkage section.
     01 StringToCheck pic x any length.
@@ -33,44 +37,40 @@ procedure division using StringToCheck returning ParenthesesAreBalancedFlag.
     end-if
 
     perform varying CurrentCharacterPosition from 1 by 1 until CurrentCharacterPosition > NumCharsInString
-        evaluate StringToCheck(CurrentCharacterPosition:1)
-            when "(" call "Stack", using by content "Push", CurrentCharacterPosition
-            when "[" call "Stack", using by content "Push", CurrentCharacterPosition
-            when "{" call "Stack", using by content "Push", CurrentCharacterPosition
-            when ")"
-                call "Stack", using by content "Peek", by reference LastUnclosedOpenerPosition
-                if StringToCheck(LastUnclosedOpenerPosition:1) not equal to "("
-                then
-                    move 0 to ParenthesesAreBalancedFlag
-                    goback
-                else
-                    move 1 to ParenthesesAreBalancedFlag
-                    call "Stack", using by content "Pop", by reference LastUnclosedOpenerPosition
-                end-if
-            when "]"
-                call "Stack", using by content "Peek", by reference LastUnclosedOpenerPosition
-                if StringToCheck(LastUnclosedOpenerPosition:1) not equal to "["
-                then
-                    move 0 to ParenthesesAreBalancedFlag
-                    goback
-                else
-                    move 1 to ParenthesesAreBalancedFlag
-                    call "Stack", using by content "Pop", by reference LastUnclosedOpenerPosition
-                end-if
-            when "}"
-                call "Stack", using by content "Peek", by reference LastUnclosedOpenerPosition
-                if StringToCheck(LastUnclosedOpenerPosition:1) not equal to "{"
-                then
-                    move 0 to ParenthesesAreBalancedFlag
-                    goback
-                else
-                    move 1 to ParenthesesAreBalancedFlag
-                    call "Stack", using by content "Pop", by reference LastUnclosedOpenerPosition
-                end-if
+
+        move StringToCheck(CurrentCharacterPosition:1) to CurrentCharacter
+
+        evaluate true
+            when CurrentCharacter is Opener perform ProcessOpenBracket
+            when CurrentCharacter is Closer perform ProcessClosedBracket
         end-evaluate
 
     end-perform
 
     goback.
+
+    ProcessOpenBracket.
+        call "Stack", using by content "Push", CurrentCharacterPosition.
+
+    ProcessClosedBracket.
+        call "Stack", using by content "Peek", by reference LastUnclosedOpenerPosition
+        move StringToCheck(LastUnclosedOpenerPosition:1) to LastOpenerCharacter
+
+        evaluate true also true
+            when CurrentCharacter is CloseRoundBracket also LastOpenerCharacter is OpenRoundBracket
+                move 1 to ParenthesesAreBalancedFlag
+                call "Stack", using by content "Pop", by reference LastUnclosedOpenerPosition
+            when CurrentCharacter is CloseSquareBracket also LastOpenerCharacter is OpenSquareBracket
+                move 1 to ParenthesesAreBalancedFlag
+                call "Stack", using by content "Pop", by reference LastUnclosedOpenerPosition
+            when CurrentCharacter is CloseBrace also LastOpenerCharacter is OpenBrace
+                move 1 to ParenthesesAreBalancedFlag
+                call "Stack", using by content "Pop", by reference LastUnclosedOpenerPosition
+            when other
+                move 0 to ParenthesesAreBalancedFlag
+                goback
+        end-evaluate
+        .
+    EndProcessCloseBracket.
 
 end function ParenthesesAreBalanced.
